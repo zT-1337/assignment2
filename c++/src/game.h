a8fe76ebbd4c07332c661288fa31e58106bda502
Thomas Zerr <thomas.zerr.3@hof-university.de>
2022-11-10 21:13:19 +0100

    -added: game header

---
+++ c++/src/game.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include "entity_manager.h"
+#include <SFML/Graphics.hpp>
+#include <memory>
+
+struct PlayerConfig 
+{
+  int shapeRadius;
+  int collisionRadius;
+  float speed;
+  int fillColorR;
+  int fillColorG;
+  int fillColorB;
+  int outlineColorR;
+  int outlineColorG;
+  int outlineColorB;
+  int outlineThickness;
+  int shapeVertices;
+};
+
+struct EnemyConfig 
+{
+  int shapeRadius;
+  int collisionRadius;
+  float minSpeed;
+  float maxSpeed;
+  int outlineColorR;
+  int outlineColorG;
+  int outlineColorB;
+  int outlineThickness;
+  int minShapeVertices;
+  int maxShapeVertices;
+  int childLifespan;
+  int spawnInterval;
+};
+
+struct BulletConfig 
+{
+  int shapeRadius;
+  int collisionRadius;
+  float speed;
+  int fillColorR;
+  int fillColorG;
+  int fillColorB;
+  int outlineColorR;
+  int outlineColorG;
+  int outlineColorB;
+  int outlineThickness;
+  int shapeVertices;
+  int lifespan;
+};
+
+class Game
+{
+
+  sf::RenderWindow m_window;
+  EntityManager m_entities;
+  sf::Font m_font;
+  sf::Text m_text;
+  PlayerConfig m_playerConfig;
+  EnemyConfig m_enemyConfig;
+  BulletConfig m_bulletConfig;
+  int m_score = 0;
+  int m_currentFrame = 0;
+  int m_lastEnemySpawnTime = 0;
+  bool m_paused = false;
+  bool m_running = true;
+
+  std::shared_ptr<Entity> m_player;
+
+  void init(const std::string & config_path);
+  void setPaused(bool paused);
+
+  void sMovement();
+  void sUserInput();
+  void sLifespan();
+  void sRender();
+  void sEnemySpawner();
+  void sCollision();
+
+  void spawnPlayer();
+  void spawnEnemy();
+  void spawnChildEnemies(std::shared_ptr<Entity> enemy);
+  void spawnBullet(std::shared_ptr<Entity> shooter, const Vec2 & mousePos);
+  void spawnSpecialWeapon(std::shared_ptr<Entity> shooter);
+
+public:
+  Game(const std::string & config_path);
+
+  void run();
+};
--- c++/src/main.cpp
+++ c++/src/main.cpp
@@ -1,36 +1,10 @@
 #include <iostream>
-#include "entity_manager.h"
+#include "game.h"
 
 int main()
 {
-  EntityManager manager;
-
-  std::cout << manager.getEntities().size() << std::endl;
-
-  auto test_entity0 = manager.addEntity("test");
-  auto test_entity1 = manager.addEntity("test");
-  auto test_entity2 = manager.addEntity("test");
-  auto test_entity3 = manager.addEntity("based");
-  std::cout << manager.getEntities().size() << std::endl;
-  std::cout << manager.getEntities("test").size() << std::endl;
-
-  manager.update();
-  std::cout << manager.getEntities().size() << std::endl;
-  std::cout << manager.getEntities("test").size() << std::endl;
-  std::cout << manager.getEntities("based").size() << std::endl;
-
-  test_entity0->destroy();
-  test_entity2->destroy();
-  test_entity3->destroy();
-  manager.update();
-  std::cout << manager.getEntities().size() << std::endl;
-  std::cout << manager.getEntities("test").size() << std::endl;
-  std::cout << manager.getEntities("based").size() << std::endl;
-
-  for(auto& e : manager.getEntities())
-  {
-    std::cout << e->id() << " " << e->tag() << std::endl;
-  }
+  Game game("config.txt");
+  game.run();
 
   return 0;
 }
