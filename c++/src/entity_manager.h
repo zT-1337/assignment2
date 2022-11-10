974099afb169d46a2c9ca1e55490ce3f2dea1800
Thomas Zerr <thomas.zerr.3@hof-university.de>
2022-11-10 19:14:29 +0100

    -added: headers for components, entities and entity manager

---
+++ c++/src/entity_manager.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "entity.h"
+#include <vector>
+#include <map>
+#include <memory>
+
+typedef std::vector<std::shared_ptr<Entity>> EntityVec;
+typedef std::map<std::string, EntityVec> EntityMap;
+
+class EntityManager
+{
+  EntityVec m_entities;
+  EntityVec m_entitiesToAdd;
+  EntityMap m_entityMap;
+  size_t m_totalEntities = 0;
+
+public:
+  EntityManager();
+
+  void update();
+  std::shared_ptr<Entity> addEntity(const std::string & tag);
+  EntityVec& getEntities();
+  EntityVec& getEntities(const std::string & tag);
+};
--- c++/src/main.cpp
+++ c++/src/main.cpp
@@ -1,69 +1,8 @@
 #include <iostream>
 #include "vec2.h"
+#include "entity_manager.h"
 
 int main()
 {
-  Vec2 a;
-  Vec2 b(50.0f, 40.0f);
-  Vec2 c(50.0f, 40.0f);
-
-  std::cout << "init" << std::endl;
-  std::cout << a.x << " " << a.y << std::endl;
-  std::cout << b.x << " " << b.y << std::endl;
-
-  std::cout << "equals" << std::endl;
-  std::cout << (a == b) << std::endl;
-  std::cout << (a != b) << std::endl;
-
-  std::cout << (c == b) << std::endl;
-  std::cout << (c != b) << std::endl;
-
-  std::cout << "addition" << std::endl;
-  Vec2 d = a + b + c;
-  std::cout << d.x << " " << d.y << std::endl;
-
-  std::cout << "subtraction" << std::endl;
-  Vec2 e = d - b;
-  std::cout << e.x << " " << e.y << std::endl;
-
-  std::cout << "multiplication with single value" << std::endl;
-  Vec2 f = b * 2;
-  std::cout << f.x << " " << f.y << std::endl;
-
-  std::cout << "division with single value" << std::endl;
-  Vec2 g = b / 2;
-  std::cout << g.x << " " << g.y << std::endl;
-
-  std::cout << "addition withouth new instance" << std::endl;
-  b += c;
-  std::cout << b.x << " " << b.y << std::endl;
-
-  std::cout << "subtraction without new instance" << std::endl;
-  b -= c;
-  std::cout << b.x << " " << b.y << std::endl;
-
-  std::cout << "multiplication with single value withouth new instance" << std::endl;
-  b *= 2;
-  std::cout << b.x << " " << b.y << std::endl;
-
-  std::cout << "division with single value withouth new instance" << std::endl;
-  b /= 2;
-  std::cout << b.x << " " << b.y << std::endl;
-
-  std::cout << "dist" << std::endl;
-  Vec2 unitVec(3.5f, 3.5f);
-  float a_b = a.dist(b);
-  float b_c = b.dist(c);
-  float a_unitVec = a.dist(unitVec);
-  std::cout << a_b << std::endl;
-  std::cout << b_c << std::endl;
-  std::cout << a_unitVec << std::endl;
-
-  std::cout << "normalize" << std::endl;
-  b.x = 3;
-  b.y = 4;
-  b.normalize();
-  std::cout << b.x << " " << b.y << " " << b.length() << std::endl;
-
   return 0;
 }
